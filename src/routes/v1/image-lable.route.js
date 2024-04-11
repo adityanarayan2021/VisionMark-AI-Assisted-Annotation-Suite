4ced95e040a0c0dc8d78922d2d1787bea8707282
Aditya Narayan <adityanarayan.proses@gmail.com>
2024-04-11 22:10:18 +0530

    vision mark assignment added read me

--- src/routes/v1/image-lable.route.js
+++ src/routes/v1/image-lable.route.js
@@ -28,3 +28,102 @@ router
 
 
 module.exports = router;
+
+
+/**
+ * @swagger
+ * tags:
+ *   name: Image
+ *   description: Image-related operations
+ */
+
+/**
+ * @swagger
+ * /images:
+ *   post:
+ *     summary: Upload an image and label it
+ *     tags: [Image]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               image:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       "200":
+ *         description: Image uploaded and labeled successfully
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+
+ *   get:
+ *     summary: Export data in CSV format
+ *     tags: [Image]
+ *     responses:
+ *       "200":
+ *         description: CSV data exported successfully
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+
+ * /images/{status}:
+ *   get:
+ *     summary: Get all images by status
+ *     tags: [Image]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: status
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Status of the images to retrieve
+ *     responses:
+ *       "200":
+ *         description: Successfully retrieved images by status
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+
+ * /images/{imageId}:
+ *   put:
+ *     summary: Update the status of an image
+ *     tags: [Image]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: imageId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the image to update
+ *     responses:
+ *       "200":
+ *         description: Image status updated successfully
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+
+ * /images/export/csv:
+ *   get:
+ *     summary: Export image data in CSV format
+ *     tags: [Image]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       "200":
+ *         description: Image data exported successfully in CSV format
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ */
+
